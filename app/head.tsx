6de359d2e44e910ece38bf440d7704c0690d1c12
katjalmarko <katjalmarko@gmail.com>
2023-02-13 18:39:32 +0100

    Building the Home Page

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>OpenAI - ChatGPT-3</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
